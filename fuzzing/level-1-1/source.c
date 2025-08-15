eeff4c45b1ec8d497a304e39ccc4abb535ea0e37
aturt13 <aturt13@disroot.org>
2025-08-15 09:39:36 +0200

    Updated instructions

--- fuzzing/level-1-1/source.c
+++ fuzzing/level-1-1/source.c
@@ -15,6 +15,8 @@ int main(int argc, char** argv) {
     puts("You can also do `man afl-fuzz` to read the manpages.\n");
     puts("Fuzzing is an effective way of finding vulnerabilities in complex codebases.");
     puts("afl-fuzz and afl-clang-fast are already installed in this dojo as they will be the primary tools for solving these challenges.");
+    puts("To use afl-fuzz, you'll need to provide it with a sample input file.");
+    puts("For this challenge, the input file can contain anything which does not crash the app.");
     puts("In this challenge, you will have the source code available. This means that you can use afl-clang-fast to compile it for a better fuzzing experience.");
     puts("This challenge reads input from stdin!");
 
